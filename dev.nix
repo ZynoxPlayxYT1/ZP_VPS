621b917b1fda9f0edaf077666900e7a61173db54
ZynoxPlayxYT1 <zynoxplayz2025@gmail.com>
2026-03-09 20:15:30 +0600

    dev.nix

--- dev.nix
+++ dev.nix
@@ -1,34 +1,45 @@
 { pkgs, ... }: {
   channel = "stable-24.05";
-
-  packages = with pkgs; [
-    unzip
-    openssh
-    git
-    qemu_kvm
-    sudo
-    cdrkit
-    cloud-utils
+  
+  # Full list of packages for a glitch-free experience
+  packages = with pkgs; [ 
+    unzip 
+    openssh 
+    git 
+    qemu_kvm 
+    sudo 
+    cdrkit 
+    cloud-utils 
     qemu
+    apt          
+    dpkg         
+    bashInteractive
+    coreutils
+    glibcLocales 
   ];
 
-  env = {
+  env = { 
     EDITOR = "nano";
+    LANG = "en_US.UTF-8"; # Ensures text displays correctly
   };
 
   idx = {
-    extensions = [
-      "Dart-Code.flutter"
-      "Dart-Code.dart-code"
+    extensions = [ 
+      "Dart-Code.flutter" 
+      "Dart-Code.dart-code" 
     ];
-
-    workspace = {
-      onCreate = { };
-      onStart = { };
+    
+    workspace = { 
+      onCreate = {
+        # Commands to run the very first time
+      }; 
+      onStart = {
+        # Commands to run every time the workspace opens
+      }; 
     };
 
-    previews = {
-      enable = false;
+    previews = { 
+      enable = false; 
     };
   };
 }
